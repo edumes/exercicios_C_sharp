38f6bf1ecb224b085107e06956be8ef9a49ee1de
edumes <66120423+edumes@users.noreply.github.com>
2022-06-03 19:25:40 -0300

    Add files via upload

---
+++ raio de um circulo/1.2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double PI = 3.14;
+            double area = 0;
+            double raio = 0;
+
+            Console.WriteLine("Digite o raio do circulo: ");
+            raio = double.Parse(Console.ReadLine());
+
+            area = PI * (raio * raio);
+
+            Console.WriteLine("a area do circulo é de: " + area);
+
+            Console.ReadKey();
+
+            
+        }
+    }
+}
---
+++ salario/1.2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hora;
+            int horaMes;
+
+            Console.WriteLine("Digite o quanto vc ganha por hora: ");
+            hora = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o numero de horas trabalhadas no mes: ");
+            horaMes = int.Parse(Console.ReadLine());
+
+            int salario = hora * horaMes;
+            Console.WriteLine("seu salario é de {0} reais", salario);
+            Console.ReadKey();
+
+            
+        }
+    }
+}
